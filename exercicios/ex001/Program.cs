e513ad9c79289d758c0ff2af4cd967264f8ff5b4
Sara Maria <saramariasantosdev@gmail.com>
2025-07-27 18:19:03 -0300

    Commit  Inicial

---
+++ exercicios/ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+string curso = "C#: criando a sua primeira aplicação";
+
+// Mostrar o conteúdo da variável curso no console.
+Console.WriteLine("Estou estudando: " + curso);
+
+// Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+string nome = "Sara";
+string sobrenome = "Santos";
+Console.WriteLine($"Aluna: {nome} {sobrenome}");
+
+// Mostrar na tela o nome dos instrutores do curso.
+string instrutor1 = "Guilherme Lima";
+string insttrutor2 = "Daniel Portugal";
+Console.WriteLine(String.Format("Professores: {0} e {1}", instrutor1, insttrutor2));
---
+++ exercicios/ex002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListaDeLinguagens
+{
+    public static void Main(string[] args)
+    {
+        // Desafio 2
+        List<string> linguagens = new List<string>();
+
+        linguagens.Add("C#");
+        linguagens.Add("Java");
+        linguagens.Add("Javascript");
+
+        // Desafio 3
+        Console.WriteLine($"Linguagem: {linguagens[0]}");
+
+        // Desafio 4
+        Console.Write("Informe a posição desejada: ");
+        string posicao = Console.ReadLine()!;
+
+        Console.WriteLine($"Linguagem escolhida: {linguagens[int.Parse(posicao)]}");
+    }
+}
