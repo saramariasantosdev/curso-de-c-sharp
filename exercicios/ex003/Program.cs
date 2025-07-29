a0b6ad9d98b3d59f65e2953086d4e32065f2e74b
Sara Maria <saramariasantosdev@gmail.com>
2025-07-29 15:16:52 -0300

    Adiciona o exercício 3

---
+++ exercicios/ex003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Criar uma lista de bandas vazia
+List<string> bandasFavoritas = new List<string>();
+
+// e adicionar suas bandas prediletas em seguida.
+bandasFavoritas.Add("Queen");
+bandasFavoritas.Add("Pink Floyd");
+bandasFavoritas.Add("The Beatles");
+bandasFavoritas.Add("Aerosmith");
+
+// Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
+foreach (string banda in bandasFavoritas)
+{
+    Console.WriteLine($"Banda {banda}");
+}
+
