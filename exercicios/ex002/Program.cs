784ebe81f8e6ed0902f4790412eaff2053e5d024
Sara Maria <saramariasantosdev@gmail.com>
2025-07-29 15:38:10 -0300

    Retira o public class e comenta o código de acordo com o exercício

--- exercicios/ex002/Program.cs
+++ exercicios/ex002/Program.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Criar uma lista vazia
+List<string> linguagens = new List<string>();
 
-public class ListaDeLinguagens
-{
-    public static void Main(string[] args)
-    {
-        // Desafio 2
-        List<string> linguagens = new List<string>();
+// Adiicionar itens na lista
+linguagens.Add("C#");
+linguagens.Add("Java");
+linguagens.Add("Javascript");
 
-        linguagens.Add("C#");
-        linguagens.Add("Java");
-        linguagens.Add("Javascript");
+// Exibir a linguagem de programação C#
+Console.WriteLine($"Linguagem: {linguagens[0]}");
 
-        // Desafio 3
-        Console.WriteLine($"Linguagem: {linguagens[0]}");
+// Exibir a linguagem de programação escolhida pelo usuário
+Console.Write("Informe a posição desejada: ");
+string posicao = Console.ReadLine()!;
 
-        // Desafio 4
-        Console.Write("Informe a posição desejada: ");
-        string posicao = Console.ReadLine()!;
-
-        Console.WriteLine($"Linguagem escolhida: {linguagens[int.Parse(posicao)]}");
-    }
-}
+Console.WriteLine($"Linguagem escolhida: {linguagens[int.Parse(posicao)]}");
